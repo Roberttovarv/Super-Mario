49e044b5e66a3e9633d42c993d9baa1593681876
Tovar <tovarobe@amazon.es>
2025-03-24 17:31:56 +0100

    Logic added

--- Map/bushes.js
+++ Map/bushes.js
@@ -1,37 +1,64 @@
-function getRamdomInt(min, max) {
+function getRandomInt(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+let bushes = []
+
+let ranges = [
+    [88, 170],
+    [383, 478],
+    [694, 848],
+    [1310, 1480]
+
+];
+
+function isInRange(x, ranges) {
+    for (let i = 0; i < ranges.length; i++) {
+        if (x >= ranges[i][0] && x <= ranges[i][1]) {
+            return true
+        }
+    }
+    return false
+}
+
+function createBushes(){
+    let lastX = 0
+    let lastX2 = 0
+    
+    for (let i = 0; i < 10; i++) {
+        let spacing, x
+        do {
+            spacing = getRandomInt(100, 250)
+            x = spacing + lastX
+        } while (isInRange(x, ranges))
+        lastX = x
+        bushes.push(x)
+
+        let spacing2, x2
+        do {
+            spacing2 = getRandomInt(100, 250)
+            x2 = spacing2 + lastX2
+        } while (isInRange(x2, ranges))
+        lastX2 = x2
+        bushes.push(x2)
+    }
+    return bushes
+}
 
 export const loadBushes = (scene) => {
+    createBushes()
 
-    const ranges = [
-        [108, 150],
-        [403, 458],
-        [714, 828],
-        [1340, 1450]
-    ];
+    for (let i = 0; i < bushes.length; i += 2) {
+        
+        scene.add.image(bushes[i], 195, 'bush1')
+        .setOrigin(0.5, 0)
+        .setScale(.5)
+        .setDepth(2)
 
-     let lastX = 0
-     let lastX2 = 0
-
-    for (let i = 0; i < 15; i++) {
-        let spacing = getRamdomInt(100, 250)
-        let spacing2 = getRamdomInt(100, 250)
-        let x = spacing + lastX
-        let x2 = spacing2 + lastX2
-
-        scene.add.image(x, 195, 'bush1')
-            .setOrigin(0, 0)
-            .setScale(.5)
-            .setDepth(2)
-
-        scene.add.image(x2, 195, 'bush2')
-            .setOrigin(0, 0)
+        scene.add.image(bushes[i+1], 195, 'bush2')
+            .setOrigin(0.5, 0)
             .setScale(.5)
             .setDepth(3)
-lastX = x
-lastX2 = x2
     }
 }
--- Map/mountains.js
+++ Map/mountains.js
@@ -3,50 +3,62 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+let mountains = []
 
-export const loadMountains = (scene) => {
-    const ranges = [
-        [108, 150],
-        [403, 458],
-        [714, 828],
-        [1340, 1450]
-    ];
+let ranges = [
+    [68, 190],
+    [363, 498],
+    [674, 868],
+    [1300, 1500]
 
+];
+
+function isInRange(x, ranges) {
+    for (let i = 0; i < ranges.length; i++) {
+        if (x >= ranges[i][0] && x <= ranges[i][1]) {
+            return true
+        }
+    }
+    return false
+}
+
+function createMountains() {
     let lastX = 0;
     let lastX2 = 0;
 
-    for (let i = 0; i < 15; i++) {
-        let x;
-        let x2;
-        let spacing = getRandomInt(100, 250);
-        let spacing2 = getRandomInt(100, 250);
+    for (let i = 0; i < 10; i++) {
+        let spacing, x
+        do {
+            spacing = getRandomInt(100, 250)
+            x = spacing + lastX
+        } while (isInRange(x, ranges))
+        lastX = x
+        mountains.push(x)
 
-        x = spacing + lastX;
-        x2 = spacing2 + lastX2;
+        let spacing2, x2
+        do {
+            spacing2 = getRandomInt(100, 250)
+            x2 = spacing2 + lastX2
+        } while (isInRange(x2, ranges))
+        lastX2 = x2
+        mountains.push(x2)
+    }
+    return mountains
+};
 
-        for (let j = 0; j < ranges.length; j++) {
-            if (x > ranges[j][0] && x < ranges[j][1]) {
-                console.log("Montaña encontrada en rango", ranges[j]);
-                let mid = (ranges[j][0] + ranges[j][1]) / 2;
-                if (x < mid) {
-                    x = ranges[j][0] - (mid - x);
-                } else {
-                    x = ranges[j][1] + (x - mid);
-                }
-            }
-        }
+export const loadMountains = (scene) => {
+    createMountains()
 
-        scene.add.image(x, 192, 'mountain1')
-            .setOrigin(0, 0)
+    for (let i = 0; i < mountains.length; i += 2) {
+        
+        scene.add.image(mountains[i], 192, 'mountain1')
+            .setOrigin(0.5, 0)
             .setScale(.5)
             .setDepth(2);
 
-        scene.add.image(x2, 175, 'mountain2')
-            .setOrigin(0, 0)
+        scene.add.image(mountains[i + 1], 175, 'mountain2')
+            .setOrigin(0.5, 0)
             .setScale(.5)
             .setDepth(1);
-
-        lastX = x;
-        lastX2 = x2;
     }
-};
+}
